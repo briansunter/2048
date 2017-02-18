05b5fc7897b0751e13be1fae4f5647d8c789ef3f
Brian Sunter <bsunter@gmail.com>
2017-02-18 12:23:44 -0800

    Progress?

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
   :plugins [[lein-cljsbuild "1.1.3"]
             [lein-figwheel "0.5.4-5"]]
 
+  :repl-options {:timeout 180000}
   :min-lein-version "2.5.0"
 
   :clean-targets ^{:protect false}
--- src/tester/core.cljs
+++ src/tester/core.cljs
@@ -7,7 +7,7 @@
 
 (def directions #{::up ::down ::right ::left})
 
-(def board-size 4)
+(def board-size 6)
 
 (def screen-width (.-innerWidth js/window))
 (def screen-height (.-innerHeight js/window))
@@ -19,10 +19,10 @@
 (s/def ::y ::within-board-size)
 (s/def ::direction directions)
 (s/def ::position (s/tuple ::within-board-size ::within-board-size))
+(s/def ::value pos-int?)
+(s/def ::tile (s/keys :req [::position ::value]))
 
-(s/def ::tile pos-int?)
-
-(s/def ::game-board (s/map-of ::position ::tile :max-count (* board-size board-size) :min-count 2))
+(s/def ::game-board (s/coll-of ::tile :max-count (* board-size board-size) :min-count 2))
 (s/def ::app-db (s/keys :req [::game-board ::direction]))
 
 (defn initial-state
@@ -31,32 +31,39 @@
 
 (defonce app-db (reagent/atom (initial-state)))
 
+(defn dec*
+  [foo]
+  (if (<= 1 foo)
+    (dec foo)
+    0))
+
 (s/fdef positions-to-move
         :args (s/cat :current-position ::position
                      :move-direction ::direction)
         :ret (s/coll-of ::position))
 
-(defn dec-pos
-  [x]
-  (if (pos? (dec x))
-    (dec x)
-    0))
-
 (defn positions-to-move
-  [[x y] move-direction]
-  (case move-direction
+  [[x y] direction]
+  (case direction
     ::up    (map (fn [new-y] [x new-y]) (range (inc y) board-size))
-    ::down  (map (fn [new-y] [x new-y]) (reverse (range (dec-pos y))))
+    ::down  (map (fn [new-y] [x new-y]) (reverse (range (dec* y))))
     ::right (map (fn [new-x] [new-x y]) (range (inc x) board-size))
-    ::left  (map (fn [new-x] [new-x y]) (reverse (range (dec-pos x))))))
+    ::left  (map (fn [new-x] [new-x y]) (reverse (range (dec* x))))))
 
 (s/fdef next-position
         :args (s/cat :board ::game-board :positions-to-check (s/coll-of ::position))
         :ret ::position)
 
 (defn next-position
-  [board positions-to-check]
-  (or (first (filter board positions-to-check)) (last positions-to-check)))
+  [board direction position]
+  #_(let postitions-to-check []
+         (or (first (filter board positions-to-check)) (last positions-to-check))))
+
+(s/def ::position-board (s/map-of ::position (s/coll-of ::position)))
+
+(s/fdef board->position-board
+        :args (s/cat :board ::game-board :direction ::direction)
+        :ret ::position-board)
 
 (defn board->position-board
   [board direction]
@@ -66,24 +73,72 @@
         :args (s/cat :board ::game-board :direction ::direction)
         :ret ::game-board)
 
-(defn sort-next-positions
-  [nps direction]
-  (case direction
-    ::up (sort-by (fn [[x y] _] y) nps)
-    ::down (reverse (sort-by (fn [[x y] _] y) nps))
-    ::right (sort-by (fn [[x y] _] x) nps)
-    ::left (reverse (sort-by (fn [[x y] _] x) nps))))
+
+(defn tile-comparator
+  [direction]
+  (fn [tile1 tile2]
+    (let [[x1 y1] (::position tile1)
+          [x2 y2] (::position tile2)]
+      (case direction
+        ::up (< y1 y2)
+        ::down (> y1 y2)
+        ::right (< x1 x2)
+        ::left (> x1 x2)))))
+
+(s/fdef sort-tiles-by-priority
+        :args (s/cat :direction ::direction :tiles (s/coll-of ::tile))
+        :ret (s/coll-of ::tile))
+
+(defn sort-tiles-by-priority
+  [direction tiles]
+  (sort-by (tile-comparator direction) tiles))
+
+;; (defn move-tile
+;;   [board direction position]
+;;   (let [new-position ]))
+
+(s/def ::tiles-to-move (s/map-of ::within-board-size (s/coll-of ::tile)))
+
+(s/fdef group-by-direction
+        :args (s/cat :direction ::direction :board ::game-board)
+        :ret ::tiles-to-move)
+
+(defn group-by-direction
+  [direction board]
+  (group-by #(-> % ::position second) board)
+  #_(cond
+    (#{::up ::down} direction)
+    (#{::right ::left} direction) (group-by #(-> % ::position first) board)))
+
+(st/check `group-by-direction)
+#_(first (s/exercise-fn `group-by-direction))
+
+(s/fdef join-first
+        :args (s/cat :tiles (s/and (s/coll-of ::tile) #(< 0 (count %))))
+        :ret (s/coll-of ::tile))
+
+(defn join-first
+  [tiles]
+  (let [[first-tile second-tile] tiles
+        new-value (+ (::value first-tile) (::value second-tile))
+        new-position (or (::position second-tile) (::position first-tile))
+        new-tile {::value new-value ::position new-position}]
+    (cons new-tile (drop 2 tiles))))
+
+(s/fdef move-direction
+        :args (s/cat :board ::game-board :direction ::direction)
+        :ret ::game-board)
 
 (defn move-direction
   [board direction]
-  (let [positions (board->position-board board direction)
-        next-positions (map (fn [[k v]] [k (next-position board v)]) positions)
-        only-moves (filter (fn [[k v]] (not-empty v)) next-positions)
-        sorted-next-positions (sort-next-positions only-moves direction)
-        updated-board (reduce (fn [b [p1 p2]] (update (dissoc b p1) p2 + (b p1))) board sorted-next-positions)
-        ]
-    updated-board
-    ))
+  (->> (group-by-direction direction board)
+       vals
+       #_(map (partial sort-tiles-by-priority direction))
+       #_(mapcat join-first)
+       ))
+
+#_(st/check `move-direction)
+#_(second (first (s/exercise-fn `move-direction)))
 
 (defn update-state
   [state [event-type & params]]
@@ -131,10 +186,10 @@
                :padding "10%"
                :justify-content "center"
                :align-items "center"}}
-      (for [location game-board
-            :let [[position tile] location
+      (for [tile game-board
+            :let [{:keys [::position ::value]} tile
                   [x y] (position->coordinates position)]]
-        ^{:key (hash location)}
+        ^{:key (hash position)}
         [:div {:style {:position "absolute"
                        :background-color "orange"
                        :border-width 1
@@ -148,7 +203,7 @@
                        :bottom y
                        :width tile-width
                        :height tile-width
-                       }}[:a (str position tile)]])]]))
+                       }}[:a (str position value)]])]]))
 
 (defn game []
   (reagent/create-class {:reagent-render (fn [_] [game-board])
