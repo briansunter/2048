443a7e0d2aad174b96995b92fb5bcedc072b3d5f
Brian Sunter <bsunter@gmail.com>
2017-03-19 22:52:03 -0700

    Refactor subs

--- src/twentyfortyeight/subs.cljs
+++ src/twentyfortyeight/subs.cljs
@@ -8,28 +8,37 @@
  (fn [db _]
    (map :id (:game-board db))))
 
+(defn first-filter
+  [pred coll]
+  (first (filter pred coll)))
+
 (s/fdef find-first-by-id
-        :args (s/cat :board ::db/game-board :id ::db/id)
-        :ret ::db/tile)
+        :args (s/cat :coll (s/coll-of (s/keys :req-un [::db/id]))
+                     :id ::db/id)
+        :ret (s/nilable ::db/tile))
+
+(defn find-first-by-id
+  [id coll]
+  (first-filter #(= id (:id %)) coll))
 
 (rf/reg-sub
  :tile-with-id
  (fn [db [_ tile-id]]
-   (first (filter #(= tile-id (:id %)) (:game-board db)))))
+   (find-first-by-id tile-id (:game-board db))))
 
 (rf/reg-sub
  :previous-tile-with-id
  (fn [db [_ tile-id]]
-   (first (filter #(= tile-id (:id %)) (first (:previous-game-boards db))))))
+   (find-first-by-id tile-id (first (:previous-game-boards db)))))
 
 (rf/reg-sub
  :tile-at-position
  (fn [db [_ position]]
-   (first (filter #(= position (:position %)) (:game-board db)))))
+   (first-filter #(= position (:position %)) (:game-board db))))
 
 (defn tile-with-id
   [tile-id game-board]
-  (first (filter #(= tile-id (:id %)) game-board)))
+  (first-filter #(= tile-id (:id %)) game-board))
 
 (s/fdef tile-is-new?
         :args (s/cat :tile-id ::db/id
